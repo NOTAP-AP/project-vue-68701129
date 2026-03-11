a828923dfefbb9114fb2f127664098fd844dec1e
NOTAP-AP <pwaas600@gmail.com>
2026-03-11 01:47:50 -0700

    Initial commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your ไอโอ๊ต"/>
+
+
+
+      <!--เนื้อหา-->
+    <div class="px-4 py-5 my-5 text-center"> <img class="d-block mx-auto mb-4" src="../assets/logo.png" alt="width 100"
+            width="100" height="100">
+        <h1 class="display-5 fw-bold text-body-emphasis"></h1>
+        <div class="col-lg-6 mx-auto">
+            <p class="lead mb-4">"Go2upSkills Thailand's no.1 online course platform. We provide a platform for experts
+                of any profession to create courses that can be offered to the public. With Go2upSkills, anybody in can
+                teach and learn online anytime, anywhere.".</p>
+            <a class="btn btn-primary btn-lg px-4 gap-3" href="couses.html">Course</a>
+                    <a class="btn btn dark border subtle btn-lg px-4 gap-3" href="register.html">Register</a>
+        </div>
+    </div>
+    
+
+
+
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
-</script>
---
+++ src/views/contact.vue
@@ -0,0 +1,37 @@
+<template>
+    
+       <div class="alert alert-warning" role="alert">
+  อย่างสีเหลือง
+       </div>
+
+
+    <div>
+
+        <h1>Show Context อย่างสีชมพู</h1>
+        <h3>Attaphon Pumsaguan</h3>
+        <p>Welcome to vue</p>
+
+    </div>
+
+</template>
+
+<script>
+
+
+
+
+export default {
+    
+}
+</script>
+<style>
+    h3{
+        color: #888edb;
+    }
+    h1{
+        color: #f0a0d8;
+    }
+    p{
+        color: #494647;
+    }
+</style>
